bdadf5997242f44e517cd39c308f69feb43664e5
Patryk Spierewka <101202344+PatrykSpierewka@users.noreply.github.com>
2023-07-20 22:28:41 +0200

    EMG_py.py
    Add EMD denoising

--- EMG_py.py
+++ EMG_py.py
@@ -60,8 +60,72 @@ def main_plot():
     emg_signal_healthy_range, emg_signal_myopathy_range, emg_signal_neuropathy_range, start = Load_data(0, 8000)
     EMG_plot_noise(19, 15, emg_signal_healthy_range, emg_signal_myopathy_range, emg_signal_neuropathy_range, 4000, start)
 
-
+'''
 main_plot()
+'''
 
+#Empirical Mode Decomposition denoising
+def EMD(emg_signal_healthy_range, emg_signal_myopathy_range, emg_signal_neuropathy_range):#Empirical mode decomposition
 
+    decomposer_healthy = pyhht.EMD(emg_signal_healthy_range) #An object is created, which will be used further for decomposition into IMFs
+    imfs_healthy = decomposer_healthy.decompose() #Decomposition of the EMG signal into its IMF harmonic components.
+    noise_window_healthy = emg_signal_healthy_range[0:500] #Selection of samples in the signal interval [a,b] representing the noise
+    thresholds_healthy = np.std(noise_window_healthy)
+    imfs_healthy_filtered = [pywt.threshold(c, thresholds_healthy) for c in imfs_healthy] #Thresholding
+    emg_signal_healthy_denoised_EMD = np.sum(imfs_healthy_filtered, axis=0) #Reconstruction of the denoised signal by summing the denoised IMF
 
+    decomposer_myopathy = pyhht.EMD(emg_signal_myopathy_range)
+    noise_window_myopathy = emg_signal_myopathy_range[2000:3000]
+    thresholds_myopathy = np.std(noise_window_myopathy)
+    imfs_myopathy = decomposer_myopathy.decompose()
+    imfs_myopathy_filtered = [pywt.threshold(c, thresholds_myopathy) for c in imfs_myopathy]
+    emg_signal_myopathy_denoised_EMD = np.sum(imfs_myopathy_filtered, axis=0)
+
+    decomposer_neuropathy = pyhht.EMD(emg_signal_neuropathy_range)
+    imfs_neuropathy = decomposer_neuropathy.decompose()
+    noise_window_neuropathy = emg_signal_neuropathy_range[0:1000]
+    thresholds_neuropathy = np.std(noise_window_neuropathy)
+    imfs_neuropathy_filtered = [pywt.threshold(c, thresholds_neuropathy) for c in imfs_neuropathy]
+    emg_signal_neuropathy_denoised_EMD = np.sum(imfs_neuropathy_filtered, axis=0)
+
+    return emg_signal_healthy_denoised_EMD, emg_signal_myopathy_denoised_EMD, emg_signal_neuropathy_denoised_EMD
+
+def EMG_plot_comparision_EMD(x_size, y_size, emg_signal_healthy_range, emg_signal_myopathy_range, emg_signal_neuropathy_range, emg_signal_healthy_denoised_EMD, emg_signal_myopathy_denoised_EMD, emg_signal_neuropathy_denoised_EMD, fs, start):#Porównanie zaszumionego sygnału z odszumionym EMD
+
+    offset = start/fs
+    time = np.arange(len(emg_signal_healthy_range)) / fs + offset
+
+    plt.figure(figsize=(x_size, y_size))
+
+    #Subplot for a healthy person
+    plt.subplot(311)
+    plt.plot(time, emg_signal_healthy_range, label='Noisy EMG signal of a healthy person')
+    plt.plot(time, emg_signal_healthy_denoised_EMD, label='Denoised EMG signal of a healthy person')
+    plt.title('EMG signal after EMD denoising')
+    plt.ylabel('Amplitude [mV]')
+    plt.legend()
+
+    #Subplot for a person with myopathy
+    plt.subplot(312)
+    plt.plot(time, emg_signal_myopathy_range, label='Noisy EMG signal of a person with myopathy')
+    plt.plot(time, emg_signal_myopathy_denoised_EMD, label='Denoised EMG signal of a person with myopathy')
+    plt.ylabel('Amplitude [mV]')
+    plt.legend()
+
+    #Subplot for a person with neuropathy
+    plt.subplot(313)
+    plt.plot(time, emg_signal_neuropathy_range, label='Noisy EMG signal of a person with neuropathy')
+    plt.plot(time, emg_signal_neuropathy_denoised_EMD, label='Denoised EMG signal of a person with neuropathy')
+    plt.xlabel('Time [s]')
+    plt.ylabel('Amplitude [mV]')
+    plt.legend()
+    plt.show()
+
+def main_EMD():
+    emg_signal_healthy_range, emg_signal_myopathy_range, emg_signal_neuropathy_range, start = Load_data(0, 8000)
+    emg_signal_healthy_denoised_EMD, emg_signal_myopathy_denoised_EMD, emg_signal_neuropathy_denoised_EMD = EMD(emg_signal_healthy_range, emg_signal_myopathy_range, emg_signal_neuropathy_range)
+    EMG_plot_comparision_EMD(19, 15, emg_signal_healthy_range, emg_signal_myopathy_range, emg_signal_neuropathy_range, emg_signal_healthy_denoised_EMD, emg_signal_myopathy_denoised_EMD, emg_signal_neuropathy_denoised_EMD, 4000, start)
+
+'''
+main_EMD()
+'''
